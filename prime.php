5c45416285bee681f43a1c2f9a6c8a926577a461
Alexander Korolkov <alexkorolkov1@gmail.com>
2024-01-05 15:03:36 +0200

    Create prime.php

---
+++ prime.php
@@ -0,0 +1,39 @@
+<?php
+ 
+ $no = 1997;
+
+ echo check_the_number_is_prime($no);
+ 
+ function check_the_number_is_prime( $no ) {
+ 	
+  if ($no < 2) {
+ 	return 0;
+  }	
+  
+  if ($no === 2) {
+  	return 3;
+  }
+   
+  $is_prime = true;
+
+  for ($i = 3; $i < $no; $i++) {
+    if ($no % $i === 0 && ($no / $i) > 1) {
+      $is_prime = false;
+    }
+    
+  }
+  
+  if (!$is_prime) {
+    return -1;
+  } else {
+  	$sum = 0;
+  	for ($i = 1; $i <= $no; $i++) {
+  	  $sum += $i;
+  	}
+  }
+ 
+ return $sum;
+
+ }
+ 
+?> 
